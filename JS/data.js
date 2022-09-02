9a5c8efce8affb87c102bada8a04da1e1aa0bd7e
Walter Liendo <walter.liendo@gmail.com>
2022-09-02 19:44:28 -0300

    data

---
+++ JS/data.js
@@ -0,0 +1,12 @@
+var data=[
+    {codigo:"cod1",
+     imagen:"imagenes/vestido.jpg",
+     nombre:"Vestido",
+     descripcion:"Vestido de seda",
+    },
+    {codigo:"cod2",
+     imagen:"imagenes/vestido.jpg",
+     nombre:"Vestido",
+     descripcion:"Vestido de seda",
+    }
+]
--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,43 @@
-(function($) { 
-  $(function() { 
-    $('nav ul li a:not(:only-child)').click(function(e) {
+(function ($) {
+  $(function () {
+    $('nav ul li a:not(:only-child)').click(function (e) {
       $(this).siblings('.nav-dropdown').toggle();
       $('.dropdown').not($(this).siblings()).hide();
       e.stopPropagation();
     });
-    $('html').click(function() {
+    $('html').click(function () {
       $('.nav-dropdown').hide();
     });
-    $('#nav-toggle').click(function() {
+    $('#nav-toggle').click(function () {
       $('nav ul').slideToggle();
     });
-    $('#nav-toggle').on('click', function() {
+    $('#nav-toggle').on('click', function () {
       this.classList.toggle('active');
     });
-  }); 
+  });
 })(jQuery);
-  
+
+if (document.getElementById("idtabla")) {
+  cad=`
+  <p> Producto <p>
+  <table>
+      <tr>
+        <th>Nombre</th>
+        <th>Descripcion</th>
+        <th>Imagen</thd>
+      </tr>
+  `
+  for (var i=0; i < data.length ; i++){
+    cad+=`
+      <tr>
+        <td>${data[i].nombre}</td>
+        <td>${data[i].descripcion}</td>
+        <td><img src="${data[i].imagen}"></td>
+      </tr>
+    `
+  }
+  cad+=`
+  </table>
+  `
+  document.getElementById("idtabla").innerHTML=cad;
+}
